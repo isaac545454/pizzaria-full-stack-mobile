b3ab86852c36fac0cfd99a9f8ca5fb0eb6e5de7a
isaac gomes <93602614+isaac545454@users.noreply.github.com>
2022-10-28 13:55:18 -0300

    jwt feito caminhando para as rotas autenticadas

--- back/src/services/user/AuthUserServices.ts
+++ back/src/services/user/AuthUserServices.ts
@@ -1,5 +1,6 @@
 import prismaClient from '../../prisma'
 import {compare} from 'bcryptjs'
+import {sign}  from 'jsonwebtoken'
 
 interface AuthRequest{
   email: string;
@@ -26,8 +27,26 @@ class AuthUserServices {
       throw new Error("email ou senha incoreto")   
     }
 
+    //criando token
+    const token = sign(
+    {
+      name: user.name,
+      email: user.email
+    },
+    process.env.JWT_SECRET, 
+    {
+      subject: user.id,
+      expiresIn: '30d'
+    }
+     )
 
-    return {ok: true}
+
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      token: token
+    }
 
   }
 }
