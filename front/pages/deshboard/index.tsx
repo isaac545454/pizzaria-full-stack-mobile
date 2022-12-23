c0209364db3a65e5c35a65707202b215e0b5c0bb
isaac545454 <isaac.newton1221@gmail.com>
2022-12-23 19:58:43 -0300

    feat: layout painel

--- front/pages/deshboard/index.tsx
+++ front/pages/deshboard/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { canSSRAuth } from "../../ultils/canSSRAuth";
 import Header from "../../components/Header";
+import { FiRefreshCcw } from "react-icons/fi";
 
 export default function Deshboard() {
   return (
@@ -10,6 +11,25 @@ export default function Deshboard() {
       </Head>
       <div>
         <Header />
+        <main className="max-w-[720px] mx-auto my-16 px-8 flex flex-col">
+          <div className="flex">
+            <h1 className="text-white font-bold text-2xl mr-4">
+              Ultimos pedidos
+            </h1>
+
+            <button>
+              <FiRefreshCcw color="#3FFFA3" size={30} />
+            </button>
+          </div>
+          <article className="flex flex-col my-4">
+            <section className="flex bg-dark-900 mb-4 items-center rounded-md">
+              <button className="bg-transparent text-white text-2xl h-[60px] items-center flex">
+                <div className="w-2 bg-green-primary h-[60px] rounded-tl-md rounded-bl-md mr-4"></div>
+                <span>mesa 40</span>
+              </button>
+            </section>
+          </article>
+        </main>
       </div>
     </>
   );
--- front/pages/product/index.tsx
+++ front/pages/product/index.tsx
@@ -1,11 +1,12 @@
 import Head from "next/head";
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 import Header from "../../components/Header";
 import { canSSRAuth } from "../../ultils/canSSRAuth";
 import { FiUpload } from "react-icons/fi";
 import Image from "next/image";
 import { setupAPIClient } from "../../services/api";
 import { type } from "os";
+import { toast } from "react-toastify";
 
 interface Props {
   categoryList: ItemProps[];
@@ -18,9 +19,12 @@ type ItemProps = {
 
 export default function index({ categoryList }: Props) {
   const [avatarUrl, setAvatarUrl] = useState<string>("");
-  const [imageUrl, setImageUrl] = useState<null | File>(null);
+  const [imageUrl, setImageUrl] = useState<File | null>(null);
   const [categories, setCategories] = useState<ItemProps[]>(categoryList);
   const [categorieSelected, setCategorieSelected] = useState<number>(0);
+  const [nameProduct, setNameProduct] = useState<string>("");
+  const [Price, setPrice] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
 
   const handleFile = (e: ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files) return;
@@ -39,6 +43,41 @@ export default function index({ categoryList }: Props) {
     setCategorieSelected(Number(e.target.value));
   };
 
+  const handleRegister = async (e: FormEvent) => {
+    e.preventDefault();
+    if (
+      !categories ||
+      !Price ||
+      !nameProduct ||
+      !description ||
+      imageUrl === null
+    ) {
+      toast.warn("Preencha todos os campos");
+    }
+
+    try {
+      if (!imageUrl) return;
+      const data = new FormData();
+      data.append("name", nameProduct);
+      data.append("price", Price);
+      data.append("description", description);
+      data.append("category_id", categories[categorieSelected].id);
+      data.append("file", imageUrl);
+
+      const apiClient = setupAPIClient();
+      await apiClient.post("/products", data);
+      toast.success("Cadastrado com sucesso!");
+      setNameProduct("");
+      setPrice("");
+      setDescription("");
+      setAvatarUrl("");
+      setCategorieSelected(0);
+    } catch (error) {
+      toast.error("Ops... houve um erro ao cadastrar");
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <Head>
@@ -48,7 +87,7 @@ export default function index({ categoryList }: Props) {
         <Header />
         <main className="max-w-[720px] my-16 mx-auto px-8 flex justify-between flex-col">
           <h1 className="text-white text-2xl">Cadastrar produto</h1>
-          <form className="flex flex-col my-4">
+          <form className="flex flex-col my-4" onSubmit={handleRegister}>
             <label className="w-full h-[280px] mb-4  rounded-md bg-dark-900 flex justify-center items-center cursor-pointer">
               <span className="z-[99] absolute opacity-40 transition-colors hover:scale-125 hover:opacity-100">
                 <FiUpload size={30} color="white" />
@@ -85,16 +124,22 @@ export default function index({ categoryList }: Props) {
               type="text "
               placeholder="DIgite o nome do produto "
               className="mb-4 text-white bg-dark-900 border border-white px-2 h-[40px] rounded-md"
+              onChange={(e) => setNameProduct(e.target.value)}
+              value={nameProduct}
             />
             <input
               type="text "
               placeholder="DIgite o Preço do produto  "
               className="mb-4 text-white bg-dark-900 border border-white px-2 h-[40px] rounded-md"
+              onChange={(e) => setPrice(e.target.value)}
+              value={Price}
             />
             <textarea
               placeholder="descreva seu produto..."
               className="w-full h-[120px] mb-4 resize-none bg-dark-900 text-white p-2 rounded-md 
               border border-white"
+              onChange={(e) => setDescription(e.target.value)}
+              value={description}
             />
             <button className="h-[40px] bg-green-primary text-2xl text-dark-900 font-bold rounded-md">
               Cadastrar
