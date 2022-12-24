84a904f791e7e2c98e9855540335903c54e81177
isaac545454 <isaac.newton1221@gmail.com>
2022-12-24 17:51:14 -0300

    feat: login mobile

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,10 +1,75 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
 import React from "react";
 
 export default function SignIn() {
   return (
-    <View>
-      <Text>SignIn</Text>
+    <View style={styles.container}>
+      <Image style={styles.logo} source={require("../../assets/logo.png")} />
+      <View style={styles.InputContainer}>
+        <TextInput
+          placeholder="Email"
+          style={styles.input}
+          placeholderTextColor="#f0f0f0"
+        />
+        <TextInput
+          placeholder="Senha"
+          style={styles.input}
+          secureTextEntry={true}
+          placeholderTextColor="#f0f0f0"
+        />
+
+        <TouchableOpacity style={styles.AreaBtn}>
+          <Text style={styles.textBtn}>Acessar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1d1d2e",
+  },
+  logo: {
+    marginBottom: 18,
+  },
+  InputContainer: {
+    width: "95%",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 32,
+    paddingHorizontal: 14,
+  },
+  input: {
+    width: "95%",
+    height: 40,
+    backgroundColor: "#101026",
+    marginBottom: 12,
+    borderRadius: 4,
+    paddingHorizontal: 8,
+    color: "#fff",
+  },
+  AreaBtn: {
+    width: "95%",
+    height: 40,
+    backgroundColor: "#3fffa3",
+    borderRadius: 4,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textBtn: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#101026",
+  },
+});
