c142b91254a45437dc29d9a81967ccf89a8653fb
isaac545454 <isaac.newton1221@gmail.com>
2022-12-25 18:52:37 -0300

    feat: Params route react native

--- mobile/src/pages/Order/index.tsx
+++ mobile/src/pages/Order/index.tsx
@@ -1,10 +1,21 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
+import { useRoute, RouteProp } from "@react-navigation/native";
+
+type Props = {
+  Order: {
+    number: string | number;
+    order_id: string;
+  };
+};
+
+type OrderRuteProps = RouteProp<Props, "Order">;
 
 export default function Order() {
+  const route = useRoute<OrderRuteProps>();
   return (
     <View style={styles.container}>
-      <Text>index</Text>
+      <Text>index {route.params.number}</Text>
     </View>
   );
 }
